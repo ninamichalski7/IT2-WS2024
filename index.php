b20a5fe48a6ec32827f54c5d4fe25f665558d4f4
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2023-09-28 17:16:18 +0200

    Create index.php
    Programm für die Berechnung der Fallzeit eines Gegenstandes

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Unbenanntes Dokument</title>
+</head>
+
+<body>
+	<h1>Das erste Programm</h1>
+	<?php
+		// Programmcode für die Berechnung der Zeit
+		/*
+		Mehrzeiliger Programm-
+		code
+		*/
+		$hoehe=10000;
+		echo(sqrt((2*$hoehe)/(9.81)));
+	
+	
+	
+	?>
+</body>
+</html>
